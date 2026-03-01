a6388a012730ff82fc65ad8f91328e6695039fef
Artem_is_env0 <artem.artyunov@env0.com>
2026-03-01 17:54:37 -0500

    Create variables.tf

---
+++ workflows/WF-00/wf00_gcp_gke/variables.tf
@@ -0,0 +1,61 @@
+variable "project_id" {
+  description = "Target project ID where GKE is created."
+  type        = string
+}
+
+variable "region" {
+  description = "Default region for provider context."
+  type        = string
+  default     = "us-east1"
+}
+
+variable "enable_gke" {
+  description = "If false, create no GKE resources."
+  type        = bool
+  default     = true
+}
+
+variable "gke_location" {
+  description = "Cluster location. Use a region for regional clusters (example: us-east1) or a zone for zonal clusters (example: us-east1-b)."
+  type        = string
+}
+
+variable "gke_cluster_name" {
+  description = "Optional. If empty, auto-named from project_id."
+  type        = string
+  default     = ""
+}
+
+variable "gke_node_count" {
+  description = "Number of nodes in the node pool."
+  type        = number
+  default     = 2
+}
+
+variable "gke_machine_type" {
+  description = "Node machine type (example: e2-medium)."
+  type        = string
+  default     = "e2-medium"
+}
+
+variable "gke_disk_size_gb" {
+  description = "Node boot disk size in GB."
+  type        = number
+  default     = 50
+}
+
+variable "gke_release_channel" {
+  description = "Optional. Release channel: RAPID, REGULAR, STABLE."
+  type        = string
+  default     = "REGULAR"
+}
+
+variable "network_self_link" {
+  description = "VPC self link from the Network component."
+  type        = string
+}
+
+variable "subnet_self_link" {
+  description = "Subnet self link from the Network component."
+  type        = string
+}
