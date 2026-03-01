ca3a7a10fae54f230aed0407be65a0a9d8570e19
Artem_is_env0 <artem.artyunov@env0.com>
2026-03-01 17:21:04 -0500

    Add outputs for project ID and project number

---
+++ workflows/WF-00/wf00_gcp_project/outputs.tf
@@ -0,0 +1,9 @@
+output "project_id" {
+  value       = local.project_id
+  description = "Target project ID (created or adopted)."
+}
+
+output "project_number" {
+  value       = local.project_number
+  description = "Target project number (created or adopted)."
+}
