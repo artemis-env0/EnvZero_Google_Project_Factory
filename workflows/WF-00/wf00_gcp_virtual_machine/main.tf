0ce3bda1867465fa54737f16e2b9081b5f404fc8
Artem_is_env0 <artem.artyunov@env0.com>
2026-03-09 17:04:10 -0400

    Update main.tf

--- workflows/WF-00/wf00_gcp_virtual_machine/main.tf
+++ workflows/WF-00/wf00_gcp_virtual_machine/main.tf
@@ -1,11 +1,10 @@
 locals {
   effective_count = var.enable_vms ? var.vm_count : 0
-  name_prefix     = "${var.project_id}-vm"
 }
 
 resource "google_compute_instance" "vm" {
   count        = local.effective_count
-  name         = "${local.name_prefix}-${count.index + 1}"
+  name         = var.vm_name_suffix != "" ? "${var.project_id}-vm-${var.vm_name_suffix}" : "${var.project_id}-vm-${count.index + 1}"
   machine_type = var.vm_machine_type
   zone         = var.vm_zone
   tags         = var.vm_tags
@@ -21,7 +20,6 @@ resource "google_compute_instance" "vm" {
   network_interface {
     subnetwork = var.subnet_self_link
 
-    # Add a public IP only when enabled
     dynamic "access_config" {
       for_each = var.enable_public_ip ? [1] : []
       content {}
