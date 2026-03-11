558a573747d46399af570c9e090387ecdbf059a0
Artem_is_env0 <artem.artyunov@env0.com>
2026-03-11 11:45:20 -0400

    Update main.tf

--- workflows/WF-00/wf00_gcp_gke/main.tf
+++ workflows/WF-00/wf00_gcp_gke/main.tf
@@ -24,6 +24,8 @@ resource "google_container_cluster" "cluster" {
 
   logging_service    = "logging.googleapis.com/kubernetes"
   monitoring_service = "monitoring.googleapis.com/kubernetes"
+
+  deletion_protection = false
 }
 
 resource "google_container_node_pool" "primary" {
