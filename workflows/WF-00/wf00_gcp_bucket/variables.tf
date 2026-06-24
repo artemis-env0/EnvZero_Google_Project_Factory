abe66f73f7a7a5a9f12d51ee8229e2ac43c81640
Artem_is_env0 <artem.artyunov@env0.com>
2026-06-24 17:48:49 -0400

    Refactor variable descriptions and add new bucket name options
    Updated variable descriptions for clarity and added new variables for bucket name suffix and exact name override.

--- workflows/WF-00/wf00_gcp_bucket/variables.tf
+++ workflows/WF-00/wf00_gcp_bucket/variables.tf
@@ -10,31 +10,43 @@ variable "region" {
 }
 
 variable "bucket_location" {
-  description = "Bucket location (multi-region like US or region like us-east1)."
+  description = "Cloud Storage bucket location."
   type        = string
   default     = "US"
 }
 
 variable "bucket_name_prefix" {
-  description = "Optional. If empty, uses <project_id>-bkt."
+  description = "Optional prefix for the bucket name. If empty, project_id is used."
+  type        = string
+  default     = ""
+}
+
+variable "bucket_name_suffix" {
+  description = "Optional suffix for the bucket name. Use this to distinguish workflow variants such as large."
+  type        = string
+  default     = ""
+}
+
+variable "bucket_name" {
+  description = "Optional exact bucket name override. If set, this value is used directly and must be globally unique."
   type        = string
   default     = ""
 }
 
 variable "force_destroy" {
-  description = "If true, bucket will be deleted even if it contains objects."
+  description = "If true, delete all objects when destroying the bucket."
   type        = bool
   default     = true
 }
 
 variable "deployer_user_email" {
-  description = "Optional. If set, grants roles/storage.admin on this bucket to the deployer user."
+  description = "Optional user email to grant bucket admin access."
   type        = string
   default     = ""
 }
 
 variable "enable_bucket" {
-  description = "If false, create no bucket."
+  description = "If false, create no bucket resources."
   type        = bool
   default     = true
 }
